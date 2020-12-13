5d9460c354f07ea266d07269e2d6603b91926cb0
Stacey <chiarahung@hotmail.com>
2020-12-13 15:53:10 +0800

    Add homepage and its css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
+    <div className="grid-container">
+      <header>
+        <a href="/">React Shopping Cart</a>
       </header>
+      <main>
+        Product List
+      </main>
+      <footer>All right is reserved</footer>
     </div>
   );
 }
